e0a7248d0bde36f71fe7ee5ea9b07c3ee6f8b92f
Rafael Hedman <rafael@rafael.se>
2021-05-11 15:52:15 +0200

    Copy an image

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, const char * argv[]) {
 	std::string sourceImageFilePath{ "/Users/raffa/Work/Star Stable/Sample images/earth.tga" };
+	std::string targetImageFilePath{ "/Users/raffa/Work/Star Stable/resample.tga" };
+
+	// Read source image file.
 	std::ifstream sourceImageFile{ sourceImageFilePath, std::ifstream::binary|std::ifstream::ate };
 
 	if (!sourceImageFile)
@@ -15,9 +18,23 @@ int main(int argc, const char * argv[]) {
 	auto sourceImageFileSize = sourceImageFile.tellg();
 	std::vector<char> sourceImageBuffer(sourceImageFileSize);
 
+	sourceImageFile.seekg(0);
 	sourceImageFile.read(sourceImageBuffer.data(), sourceImageFileSize);
 
-	std::cout << sourceImageBuffer.size() << '\n';
+	std::cout << "Source image file read.\n";
+
+	// Write target image file.
+	std::ofstream targetImageFile{ targetImageFilePath, std::ifstream::binary };
+
+	if (!targetImageFile)
+	{
+		std::cerr << "Target image file could not be opened for writing.\n";
+		return 1;
+	}
+
+	targetImageFile.write(sourceImageBuffer.data(), sourceImageFileSize);
+
+	std::cout << "Target image file written.\n";
 
 	return 0;
 }
