ef2018303d6fc36170e12a1ff97c5fbce0507ee4
Rafael Hedman <rafael@rafael.se>
2021-05-11 15:25:25 +0200

    Read a file into a vector buffer

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(int argc, const char * argv[]) {
-	std::cout << "Hello, World!\n";
+	std::string sourceImageFilePath{ "/Users/raffa/Work/Star Stable/Sample images/earth.tga" };
+	std::ifstream sourceImageFile{ sourceImageFilePath, std::ifstream::binary|std::ifstream::ate };
+
+	if (!sourceImageFile)
+	{
+		std::cerr << "Source image file could not be opened for reading.\n";
+		return 1;
+	}
+ 
+	auto sourceImageFileSize = sourceImageFile.tellg();
+	std::vector<char> sourceImageBuffer(sourceImageFileSize);
+
+	sourceImageFile.read(sourceImageBuffer.data(), sourceImageFileSize);
+
+	std::cout << sourceImageBuffer.size() << '\n';
+
 	return 0;
 }
